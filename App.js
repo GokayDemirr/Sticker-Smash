6071c51ec302f241315413ac11202f2c7675071b
GokayDemir <gokaydemirpc@gmail.com>
2024-07-01 15:23:17 +0300

    Initial design implement

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar style="auto" />
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
       </View>
 
       <Text style={{ color: "#fff" }}>
         Open up App.js to start working on your app!
       </Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -28,5 +33,9 @@ const styles = StyleSheet.create({
   imageContainer: {
     flex: 1,
     paddingTop: 58,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
